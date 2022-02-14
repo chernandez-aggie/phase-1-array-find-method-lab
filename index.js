1a9da83a0b5e14941d90343c80996530a58702b8
candacehernandez <chernandez.aggie@gmail.com>
2022-02-14 12:24:48 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(teamHistory){
+    for(const item of teamHistory){
+        if(item.result === 'W'){
+            return item.year;
+        }
+     }
+}
